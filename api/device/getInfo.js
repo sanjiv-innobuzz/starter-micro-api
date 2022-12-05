483b6ebe425de6fb888f819ecb357493339b62f8
sanjiv-innobuzz <81017947+sanjiv-innobuzz@users.noreply.github.com>
2022-12-05 07:36:23 +0000

    first api testing

--- api/device/getInfo.js
+++ api/device/getInfo.js
@@ -3,6 +3,7 @@ const express = require("express");
 const getInfo = async (req, res) => {
   try {
     console.log("hello");
+    res.send("getInfo called");
   } catch (error) {
     console.log(error);
   }
